599396c92289c566df96b6866639af83da22025d
Fukaya Temma <ride.or.die.2215@gmail.com>
2018-05-17 17:22:13 +0900

    add dependency gems

--- lib/ruboty/yasumimasu.rb
+++ lib/ruboty/yasumimasu.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 
+require 'google_drive'
+require 'google/api_client'
+require 'google/api_client/client_secrets'
+require 'google/api_client/auth/installed_app'
 require 'ruboty/yasumimasu/version'
